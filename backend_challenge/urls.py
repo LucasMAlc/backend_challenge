99d677be5ec1430667cc070c97edb152e72a3616
Lucas Moura Alcantara <lucasmoura404@gmail.com>
2025-10-15 19:12:02 -0300

    some changes

--- backend_challenge/urls.py
+++ backend_challenge/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from careers.views import CareerViewSet
+
+router = DefaultRouter(trailing_slash=True)
+router.register(r'careers', CareerViewSet, basename='career')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls')),
+    path('', include(router.urls)),
 ]
